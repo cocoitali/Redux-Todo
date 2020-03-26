819bf3b4a4b395ca06eb0af1d3a0d29c3a5c5016
Courtney Buratto <cmburatto@gmail.com>
2020-03-26 09:21:55 -0700

    Created deleteTodo function, passed down via props

--- todo/src/Containers/TodoContainer.js
+++ todo/src/Containers/TodoContainer.js
@@ -1,43 +1,46 @@
-import React from "react";
-import { connect } from "react-redux";
-import { addTodo, toggleTodo } from "../actions";
-import Todos from '../components/Todos';
+import React from 'react'
+import { connect } from 'react-redux'
+import { addTodo, toggleTodo, deleteTodo } from '../actions'
+import Todos from '../components/Todos'
 
 class TodoContainer extends React.Component {
-  state = {
-    data: ""
-  };
+	state = {
+		data: ''
+	}
 
-  inputHandler = e => this.setState({ data: e.target.value });
+	inputHandler = e => this.setState({ data: e.target.value })
 
-  addHandler = e => {
-    e.preventDefault();
-    this.props.addTodo(this.state.data);
-  };
+	addHandler = e => {
+		e.preventDefault()
+		this.props.addTodo(this.state.data)
+	}
 
-  
-  toggleTodo = index => {
-    this.props.toggleTodo(index);
-  };
+	toggleTodo = index => {
+		this.props.toggleTodo(index)
+	}
 
-  render() {
-    return (
-      <Todos
-        inputHandler={this.inputHandler}
-        placeholder="...add todo"
-        toggleTodo={this.toggleTodo}
-        todos={this.props.todos}
-        data={this.state.data}
-        addHandler={this.addHandler}
-      />
-    );
-  }
+	deleteTodo = e => {
+		e.preventDefault()
+		this.props.deleteTodo(this.state.data)
+	}
+
+	render() {
+		return (
+			<Todos
+				inputHandler={this.inputHandler}
+				placeholder='...add todo'
+				toggleTodo={this.toggleTodo}
+				todos={this.props.todos}
+				data={this.state.data}
+				addHandler={this.addHandler}
+				deleteTodo={this.deleteTodo}
+			/>
+		)
+	}
 }
-  
 
-const mapStateToProps = state => ({ todos: state.todos });
+const mapStateToProps = state => ({ todos: state.todos })
 
-export default connect(
-  mapStateToProps,
-  { addTodo, toggleTodo }
-)(TodoContainer);
+export default connect(mapStateToProps, { addTodo, toggleTodo, deleteTodo })(
+	TodoContainer
+)
--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -1,28 +1,19 @@
-import React from "react";
-
-
-
-
+import React from 'react'
 
 const Todos = props => {
-  return (
-    <div>
-      <input type="text" value={props.data} onChange={props.inputHandler} />
-      <button onClick={props.addHandler}>add todo</button>
-      <ul>
-        {props.todos.map((todo, index) => (
-          <li onClick={() => props.toggleTodo(index)} key={index}>
-            {todo.value}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-export default Todos;
-
-// const mapStateToProps = state => ({ todos: state.todos });
-
-// export default connect(
-//   mapStateToProps
-// )(Todos);
+	return (
+		<div>
+			<input type='text' value={props.data} onChange={props.inputHandler} />
+			<button onClick={props.addHandler}>add todo</button>
+			<button onClick={props.deleteTodo}>delete todo</button>
+			<ul>
+				{props.todos.map((todo, index) => (
+					<li onClick={() => props.toggleTodo(index)} key={index}>
+						{todo.value}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+export default Todos
