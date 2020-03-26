7b2bf81f645ab67b71ecb9d61cb4305c7877de59
Courtney Buratto <cmburatto@gmail.com>
2020-03-26 10:03:52 -0700

    cleaned up ADD_TODO reducer

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,40 +1,37 @@
-import { ADD_TODO, TOGGLE_TODO } from "../actions";
+import { ADD_TODO, TOGGLE_TODO, DELETE_TODO } from '../actions'
 
 const initialState = {
-  todos: [
-    {
-      value: "test todo",
-      completed: false
-    }
-  ]
-};
+	todos: [
+		{
+			value: 'test todo',
+			completed: false
+		}
+	]
+}
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return {
-        ...state,
-        todos: [...state.todos, action.payload]
-      };
+	switch (action.type) {
+		case ADD_TODO:
+			return [...state.todos, action.payload]
+		
+		case TOGGLE_TODO:
+			return {
+				...state,
+				todos: state.todos.map(
+					(todo, index) => {
+						if (index === action.payload) {
+							return { ...todo, completed: !todo.completd }
+						} else {
+							return todo
+						}
+					}
+					// index === action.payload
+					//   ? { ...todo, completed: !todo.completed }
+					//   : todo
+				)
+			}
 
-    case TOGGLE_TODO:
-      return {
-        ...state,
-        todos: state.todos.map(
-          (todo, index) => {
-            if (index === action.payload) {
-              return { ...todo, completed: !todo.completd };
-            } else {
-              return todo;
-            }
-          }
-            // index === action.payload
-            //   ? { ...todo, completed: !todo.completed }
-            //   : todo
-        )
-      };
-
-    default:
-      return state;
-  }
-};
+		default:
+			return state
+	}
+}
